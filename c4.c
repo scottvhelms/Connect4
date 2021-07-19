96cc058a71d1571559d7440f26a9ae143913386b
Scott Helms <scott.v.helms@gmail.com>
2021-07-19 14:11:08 -0400

    added more prototypes

--- c4.c
+++ c4.c
@@ -14,7 +14,13 @@ void disableRaw(); /*returns to previous environment after extiting program*/
 void initArray(int array[][7]); /* creates 7x7 array and populates with EMPTY tokens*/
 void drawGameBoard();	/* Graphically represents game to terminal in ASCII characters */
 
+	/*** input ***/
+
+
 	/*** Gameplay loop***/
+void moveToken(); /*moves cursor above game board to drop token*/
+void tokenPresent(); /*verify if toke is present and if so displays token correctly*/
+void drop(); /*drops token at the bottom of the array or stacks*/
 int connectFourPresent(); /*searches from right to left, bottom to top for connect 4*/
 
 
@@ -22,16 +28,18 @@ int connectFourPresent(); /*searches from right to left, bottom to top for conne
 
 
 
-
+/***GameData***/
 enum token {EMPTY = -1, YELLOW, RED};
+int gameData[7][7];
 
 
 
 /*** main ***/
 int main()
 {
-	int gameData[7][7];
 	initArray(gameData);	
+
+	
 
 	
 
