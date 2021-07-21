2d66e126dbcc0f70fa3483aaa7df17cc04099c2e
Scott Helms <scott.v.helms@gmail.com>
2021-07-21 12:08:46 -0400

    enable raw mode, diable raw mode, enabled key reader and ctrl-q to quit

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
+// Connect Four 
+// Author: Scott Helms 
+
+#include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
 
 
 
@@ -7,38 +16,41 @@
 
 
 /*** prototypes ***/
-	/*** init ***/
 
-void enableRaw(); /*clears screen upon entering program*/
-void disableRaw(); /*returns to previous environment after extiting program*/
-void initArray(int array[][7]); /* creates 7x7 array and populates with EMPTY tokens*/
-void drawGameBoard(int array[][7]);	/* Graphically represents game to terminal in ASCII characters */
+
+	/*** init ***/
+void enableRaw(); 				//clears screen upon entering program
+void disableRaw(); 				//returns to previous environment after extiting program
+void initArray(int array[][7]); 		// creates 7x7 array and populates with EMPTY tokens
+void drawGameBoard(int array[][7]);		// Graphically represents game to terminal in ASCII characters 
+void die(const char *s); 			//error reader upon failure
 
 	/*** input ***/
-void moveCursor();  /*moves cursor accross 7 possible zones above board */
-void moveFloatingToken(); /*moves representation of token to drop under cursor */
+void moveCursor();  				//moves cursor accross 7 possible zones above board 
+void moveFloatingToken(); 			//moves representation of token to drop under cursor 
 
 	/*** Gameplay loop***/
-void moveToken(); /*moves cursor above game board to drop token, also contains logic for quitting*/
-char tokenPresent(int array[][7], int x, int y);/*verify if toke is present and if so displays token correctly*/
-void drop(); /*drops token at the bottom of the array or stacks*/
-int connectFourPresent(); /*searches from right to left, bottom to top for connect 4*/
+void moveToken(); 				//moves cursor above game board to drop token
+char tokenPresent(int array[][7], int x, int y);//verify if toke is present and if so displays token correctly
+void drop(); 					//drops token at the bottom of the array or stacks
+int connectFourPresent(); 			//searches from right to left, bottom to top for connect 4
 
 	/*** Game wrapup***/
-void endGame(); /*endgame upon winning */
-void quitGame(); /*quitting with ctrl-q */
-void resetGame(); /*endgame and reinit if players want to replay*/
+void endGame(); 				//endgame upon winning 
+void quitGame(); 				//quitting with ctrl-q 
+void resetGame(); 				//endgame and reinit if players want to replay
 
 	/*** Status Bars***/
-void directionsSB(); /*normally displayed directions*/
-void playerTurnSB(); /*changes depending on players turn*/
-void winnerSB();     /*upon connect four present true*/
+void directionsSB(); 				//normally displayed directions
+void playerTurnSB(); 				//changes depending on players turn
+void winnerSB();     				//upon connect four present true
 
 
 
 /***GameData***/
-enum token {EMPTY = -1, YELLOW, RED};
-
+#define CTRL_KEY(k) ((k) & 0x1f)
+enum token {EMPTY = -1, RED, YELLOW};
+struct termios orig_termios;
 
 
 /*** main ***/
@@ -48,18 +60,18 @@ int main()
 	int gameData[7][7];
 	//int moveCounter = 0;  /* might need to track move, could be used for to decide whose turn it is */
 
-
+	enableRaw();
 	initArray(gameData);	
-
-//	while(1)
-//	{
+	
+	while(1)
+	{
 		/* drawGameBoard is prior to connectFourPresent so that upon */
 		/* a winning move the last token placement is displayed */
 		
 		drawGameBoard(gameData);		
 		connectFourPresent();
-		moveToken();	
-//	}	
+		moveCursor();	
+	}	
 
 	return 0;
 
@@ -72,14 +84,26 @@ int main()
 
 void enableRaw()
 {
+	if(tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tsgetattr");
+	atexit(disableRaw);
 
+	struct termios raw = orig_termios;
+
+	raw.c_iflag &= ~( BRKINT | ICRNL | INPCK | ISTRIP | IXON );
+	raw.c_oflag &= ~( OPOST );
+	raw.c_cflag |= ( CS8 );
+	raw.c_lflag &= ~( ECHO | ICANON | IEXTEN | ISIG );
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");	
 }
 
 
 
 void disableRaw()
 {
-
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
 }
 
 
@@ -92,7 +116,7 @@ void initArray(int array[][7])
 	{
 		for (j=0; j<7; j++)
 		{
-			array[i][j] = EMPTY;
+			array[i][j] = RED; //FIXME should be EMPTY
 		}
 	} 
 } 
@@ -133,6 +157,8 @@ void drawGameBoard(int array[][7])
 
 
 	int i,j;
+	int counterx = 0;
+	int countery = 0;
 	for (i=0; i<15; i++)
 	{
 		if(i%2 == 0) printf("+---+---+---+---+---+---+---+\n");
@@ -141,15 +167,15 @@ void drawGameBoard(int array[][7])
 			for(j=0;j<29;j++)
 			{
 				if(j%4 == 0) printf("|");
-				else if( (j-2)%4 == 0) printf("%d", 0);
-				else printf(" ");
-
-					
+				else if( (j-2)%4 == 0) printf("%d", array[counterx][countery]);
+				else printf(" ");	
 			}
+			countery = 0;
+			counterx++;
 			printf("\n");
 
 		}
-
+		
 
 
 	}
@@ -158,11 +184,32 @@ void drawGameBoard(int array[][7])
 }
 
 
+void die(const char *s)
+{
+	perror(s);
+	exit(1);
+}
+
+
 /*** Input ***/
 
 void moveCursor()
 {
+	int nread;
+	char c;
 
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if(nread == -1 && errno != EAGAIN) die("read");
+	}
+
+	switch (c)
+	{
+		case CTRL_KEY('q'):
+			exit(0);
+			break;
+	
+	}
 }
 
 
