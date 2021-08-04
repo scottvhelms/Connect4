3c13823cea712928510d633dd4c98a4b669e1775
Scott Helms <scott.v.helms@gmail.com>
2021-08-04 13:45:18 -0400

    Fixed comments to be in full sentences, fixed documentation url, and removed leading newlines.

--- main.c
+++ main.c
@@ -45,91 +45,101 @@ typedef struct GameDataElements {
 
 /** init **/
 
-// clears screen upon entering program
+// This function clears screen upon entering program.
 void enableRawInputMode(GameDataElements* GameData);
 
-// returns to previous environment after exiting program
+// This function returns to previous terminal environment after exiting program.
 void disableRawInputMode(GameDataElements* GameData);
 
-// initializes array, move counter and screen_rows and screen_cols in GameData
-// struct
+// This function initializes the elements of the GameData struct.
 void initGameData(GameDataElements* GameData);
 
-// Graphically represents game to terminal in ASCII characters
+// This function graphically represents game in the terminal in ASCII
+// characters.
 void drawGameBoard(GameDataElements* GameData);
 
-// gets terminal size to center display
+// This function gets the terminal size, which is used to center display.
 int getWindowSize(int* out_rows, int* out_cols);
 
-// error reader upon failure
+// This function reads an error name upon program failure.
 void die(GameDataElements* GameData, const char* s);
 
 /** Gameplay loop **/
 
-// moves cursor above game board to drop token
+// This function moves cursor above game board in order to drop token.
 void playerInput(GameDataElements* GameData);
 
-// verify if token is present and if so displays token correctly
+// This function verifies if atoken is present at the y and x index.
+// If token is present, it is displayed on the screen.
 char* tokenAtIndex(GameDataElements* GameData, int y, int x);
 
-// drops token at the bottom of the array or stacks
+// This function drops token to the bottom of the array or stacks on top of
+// previously placed tokens.
 int drop(GameDataElements* GameData, int index);
 
-// searches from right to left, bottom to top for connect 4
+// This function searches for the presence of a four tokens in a line
+// hoizontally, diagonally and vertically.
 int connectFourPresent();
 
 /** Game wrapup **/
 
-// endgame upon winning
+//  This function displays the endgame status bars and menu upon a player
+//  winning.
 void endGame();
 
-// quitting with ctrl-q
+// This function exits the program when the players using Ctrl-q.
 void quitGame();
 
-// endgame and reinit if players want to replay
+// This function resets the GameData array and displays a fresh game display.
 void resetGame();
 
 /** Status Bars **/
 
-// normally displayed directions
+// This function displays a status bar that contains the directions for playing
+// the game.
 void directionsStatusBar();
 
-// changes depending on players turn
+// This function displays the status bar that changes between player 1 and 2,
+// depending on players turn.
 void playerTurnStatusBar();
 
-// upon connect four present true
+// This function displays a status bar upon a connect four being found and the
+// game being over.
 void winnerStatusBar();
 
 /** Misc **/
 
-// Helper function for constructing and executing write()
+// This function is a helper function for constructing and executing write() via
+// <unistd.h>.
 void moveCursor(int amount, char* direction);
 
-// Helper function for displaying strings to game by executing write
+// This function ia a helper function for displaying strings to game display by
+// executing write().
 void display(char* item);
 
-// Helper funtion the clears screen
+// This function is a helper funtion that the clears the screen.
 void clearScreen();
 
-// Helper funtion the hides the cursor
+// This function is a helper funtion that hides the cursor.
 void hideCursor();
 
-// Helper function that unhids the cursor
+// This function is ahHelper function that unhids the cursor.
 void unhideCursor();
 
 /*** init ***/
 
 void enableRawInputMode(GameDataElements* GameData) {
-  // error processing
+  // This is used for error processing incase of a failure with tcgetattr.
   if (tcgetattr(STDIN_FILENO, &GameData->E.orig_termios) == -1) {
-    die(GameData, "tsgetattr");
+    die(GameData, "tcgetattr");
   }
 
   struct termios raw = GameData->E.orig_termios;
-  // termios.h flag documentation:
-  // pubs.opengroup.org/onlinepubs/000095399/basedef/termios.h.html BRKINT:
-  // misc flag, ICRNL: ctrl-m, INPCK: misc flag, ISTRIP: misc flag, IXON:
-  // ctrl_s and ctrl_q
+  // Documentation for termios.h flags:
+  // pubs.opengroup.org/onlinepubs/000095399/basedefs/termios.h.html
+  //
+  // BRKINT: misc flag, ICRNL: ctrl-m, INPCK: misc flag,
+  // ISTRIP: misc flag, IXON: ctrl_s and ctrl_q
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   // OPOST: output processing /r/n
   raw.c_oflag &= ~(OPOST);
@@ -138,7 +148,7 @@ void enableRawInputMode(GameDataElements* GameData) {
   // ECHO:Print text to screen, ICANON:Canonical Mode, IEXTEN & ISIG:ctrl=c
   // and ctrl-v
   raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-  // timeout for read()
+  // This enables the timeout for read().
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
@@ -154,11 +164,10 @@ void disableRawInputMode(GameDataElements* GameData) {
 }
 
 void initGameData(GameDataElements* GameData) {
-
-  // sets move counter to 0
+  // Initializes move_counter to 0.
   GameData->move_counter = 0;
 
-  // populate array with 49 EMPTY tokes
+  // Populates array with 49 EMPTY tokes.
   int i, j;
   for (i = 0; i < 7; i++) {
     for (j = 0; j < 7; j++) {
@@ -166,24 +175,19 @@ void initGameData(GameDataElements* GameData) {
     }
   }
 
-  // determine screen size
+  // Determine terminal window size to center the game display.
   if (getWindowSize(&GameData->E.screen_rows, &GameData->E.screen_cols) == -1) {
     die(GameData, "getWindowSize");
   }
 }
 
 void drawGameBoard(GameDataElements* GameData) {
-  // hides cursor
   hideCursor();
-
-  // clears screen
   clearScreen();
-
-  // return cursor to 1,1
   moveCursor(0, CORNER);
 
-  // centers and moves cursor to the beginning of the game screen, and writes
-  // "CONNECT FOUR"
+  // Centers and moves cursor to the beginning of the game screen, and writes
+  // "CONNECT FOUR".
   int i, j;
   for (i = 0; i < (GameData->E.screen_rows / 2) - 13; i++) {
     moveCursor(1, DOWN);
@@ -193,12 +197,11 @@ void drawGameBoard(GameDataElements* GameData) {
   }
   write(STDOUT_FILENO, "CONNECT FOUR", 12);
 
-  // moves down to where the board will be
+  // Moves cursor to where the board will be.
   moveCursor(7, DOWN);
-
   moveCursor(21, LEFT);
 
-  // draws game board
+  // Draws the game board.
   int counter_x = 0;
   int counter_y = 0;
   for (i = 0; i < 15; i++) {
@@ -221,7 +224,7 @@ void drawGameBoard(GameDataElements* GameData) {
     moveCursor(29, LEFT);
   }
 
-  // draws status bars
+  // Displays the status bars.
   directionsStatusBar();
   playerTurnStatusBar();
   winnerStatusBar();
@@ -240,11 +243,8 @@ int getWindowSize(int* out_rows, int* out_cols) {
 }
 
 void die(GameDataElements* GameData, const char* s) {
-  // clear screen
   clearScreen();
-  // move cursor to upper left hand corner
   moveCursor(0, CORNER);
-  // unhide cursor
   unhideCursor();
 
   disableRawInputMode(GameData);
@@ -256,7 +256,7 @@ void die(GameDataElements* GameData, const char* s) {
 /*** Gameplay Loop ***/
 
 void playerInput(GameDataElements* GameData) {
-  // establishes token type by whose turn it is
+  // Establishes token type by player turn.
   char* token;
   if (GameData->move_counter % 2 == 0) {
     token = "X";
@@ -264,13 +264,13 @@ void playerInput(GameDataElements* GameData) {
     token = "O";
   }
 
-  // places cursor above graphic representation of array[0][0]
+  // Places the cursor above graphic representation of array[0][0].
   moveCursor(15, UP);
   moveCursor(2, RIGHT);
   display(token);
   moveCursor(1, LEFT);
 
-  // loop for player entry
+  // Loop that contains all player commands to the game.
   int nread;
   char c;
   int player_turn = 1;
@@ -281,8 +281,8 @@ void playerInput(GameDataElements* GameData) {
         die(GameData, "read");
       }
     }
-    // if arrow key was used, figures out which one (finding the third
-    // character)
+    // If arrow key was used, figures out which one (finding the third
+    // character).
     if (c == '\x1b') {
       if (read(STDIN_FILENO, &c, 1) == -1) {
         die(GameData, "read");
@@ -295,22 +295,19 @@ void playerInput(GameDataElements* GameData) {
       }
     }
 
-    // for exiting the game
+    // For exiting the game using Ctrl-q.
     switch (c) {
     case CTRL_KEY('q'):
-      // clear screen
       clearScreen();
-      // move cursor to upper left hand corner
       moveCursor(0, CORNER);
-      // unhide cursor
       unhideCursor();
       disableRawInputMode(GameData);
       exit(0);
       break;
 
-    // right arrow
+    // 'C' represents the right arrow.
     case 'C':
-      // wrap logic
+      // If at the end of the array, wraps to the front of the array.
       if (index == 6) {
         display(" ");
         moveCursor(25, LEFT);
@@ -327,9 +324,9 @@ void playerInput(GameDataElements* GameData) {
 
       break;
 
-    // left arrow
+    // 'D' represents the left arrow.
     case 'D':
-      // wrap logic
+      // If at the front of the array, wraps to the end of the array.
       if (index == 0) {
         display(" ");
         moveCursor(23, RIGHT);
@@ -345,7 +342,8 @@ void playerInput(GameDataElements* GameData) {
       }
 
       break;
-    // enter , drops token and end players turn
+    // '/r' represents the enter key. This drops the token and end the players
+    // turn.
     case '\r':
       if (drop(GameData, index)) {
         player_turn--;
@@ -372,7 +370,6 @@ char* tokenAtIndex(GameDataElements* GameData, int y, int x) {
 }
 
 int drop(GameDataElements* GameData, int index) {
-
   if (GameData->array[0][index] != EMPTY) {
     return 0;
   }
@@ -412,7 +409,6 @@ void winnerStatusBar() {}
 /*** Misc ***/
 
 void moveCursor(int amount, char* direction) {
-
   char esc[10] = ESC;
   if (amount > 1) {
     char buffer[10];
@@ -442,8 +438,8 @@ int main() {
   initGameData(&GameData);
 
   while (1) {
-    // drawGameBoard is prior to connectFourPresent so that upon a winning move
-    // the last token placement is displayed
+    // The function drawGameBoard is prior to the function connectFourPresent so
+    // that upon a winning move the last token placement is displayed.
     drawGameBoard(&GameData);
     connectFourPresent(&GameData);
     playerInput(&GameData);
